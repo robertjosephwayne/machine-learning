2549ca75cda1e4ad44ff8a8c496fe6e13be0c6b0
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-12-30 17:47:42 +0700

    Add example of a 2D scatter plot with matplotlib

--- intro/matplotlib-intro.py
+++ intro/matplotlib-intro.py
@@ -7,3 +7,11 @@ plt.plot(my_array)
 plt.xlabel('some x axis')
 plt.ylabel('some y axis')
 plt.show()
+
+# basic scatter plot
+x = numpy.array([1, 2, 3])
+y = numpy.array([2, 4, 6])
+plt.scatter(x, y)
+plt.xlabel('some x axis')
+plt.ylabel('some y axis')
+plt.show()

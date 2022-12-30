b73eab0aaa88a68e56161ac365001b8d5bfe856d
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-12-30 17:42:00 +0700

    Add examples of arithmetic with numpy arrays

--- numpy-intro.py
+++ numpy-intro.py
@@ -15,3 +15,9 @@ print("First row: %s" % my_array[0])
 print("Last row: %s" % my_array[-1])
 print("Specific row and column: %s" % my_array[0, 2])
 print("Whole column: %s" % my_array[:, 2])
+
+# arithmetic
+my_array_1 = numpy.array([2, 2, 2])
+my_array_2 = numpy.array([3, 3, 3])
+print("Addition: %s" % (my_array_1 + my_array_2))
+print("Multiplication: %s" % (my_array_1 * my_array_2))

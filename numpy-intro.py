7991276e1cc447f09c51f7f3bcee4309d4642c39
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-12-30 17:40:00 +0700

    Access values

--- numpy-intro.py
+++ numpy-intro.py
@@ -5,3 +5,13 @@ my_list = [1, 2, 3]
 my_array = numpy.array(my_list)
 print(my_array)
 print(my_array.shape)
+
+# access values
+my_list = [[1, 2, 3], [3, 4, 5]]
+my_array = numpy.array(my_list)
+print(my_array)
+print(my_array.shape)
+print("First row: %s" % my_array[0])
+print("Last row: %s" % my_array[-1])
+print("Specific row and column: %s" % my_array[0, 2])
+print("Whole column: %s" % my_array[:, 2])

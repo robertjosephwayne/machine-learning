a59132fbb4bfc908392fb465081d49be4e8c3669
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-12-30 17:54:46 +0700

    Add example of pandas data frame

--- intro/pandas-intro.py
+++ intro/pandas-intro.py
@@ -8,3 +8,13 @@ my_series = pandas.Series(my_array, index=row_names)
 print(my_series)
 print(my_series[0])
 print(my_series['a'])
+
+# data frame
+my_array = numpy.array([[1, 2, 3], [4, 5, 6]])
+row_names = ['a', 'b']
+column_names = ['one', 'two', 'three']
+my_data_frame = pandas.DataFrame(my_array, index=row_names, columns=column_names)
+print(my_data_frame)
+
+print("one column: \n%s" % my_data_frame['one'])
+print("one column: \n%s" % my_data_frame.one)

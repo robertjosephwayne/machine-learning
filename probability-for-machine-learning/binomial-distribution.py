281344f03a52b345c21010b9714c91ae08837485
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-01-07 22:02:00 +0700

    Add examples of binomial process

---
+++ probability-for-machine-learning/binomial-distribution.py
@@ -0,0 +1,31 @@
+# Example of simulating a binomial process and counting success
+from numpy.random import binomial
+from scipy.stats import binom
+
+# Define the parameters of the distribution
+
+# Probability
+p = 0.3
+
+# Number of trials
+k = 100
+
+# Run a single simulation
+success = binomial(k, p)
+
+print('Total Success: %d' % success)
+
+# Calculate moments
+mean, var, _, _ = binom.stats(k, p, moments='mvsk')
+print('Mean=%.3f, Variance=%.3f' % (mean, var))
+
+# Define the distribution
+dist = binom(k, p)
+
+# Calculate the probability of n successes
+for n in range(10, 110, 10):
+    print('P of %d success: %.3f%%' % (n, dist.pmf(n)*100))
+
+# Calculate the probably of <=n successes
+for n in range(10, 110, 10):
+    print('P of %d success: %.3f%%' % (n, dist.cdf(n)*100))

457af26b9449af1b5f0379086c3d66fffb9dbaea
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-09 17:10:41 +0700

    Add example of converting between probability and log-odds

--- probability-for-machine-learning/logistic-regression-log-odds.py
+++ probability-for-machine-learning/logistic-regression-log-odds.py
@@ -1,3 +1,6 @@
+from math import log
+from math import exp
+
 # Example of converting between probability and odds
 
 # Define probability of success
@@ -11,3 +14,22 @@ print('Odds %.1f' % odds)
 # Convert back to probability
 prob = odds / (odds + 1)
 print('Probability %.1f' % prob)
+
+# Example of converting between probability and log-odds
+
+# Define probability of success
+prob = 0.8
+print('Probability %.1f' % prob)
+
+# Convert probability to odds
+odds = prob / (1 - prob)
+print('Odds %.1f' % odds)
+
+# Convert odds to log-odds
+logodds = log(odds)
+print('Log-Odds %.1f' % logodds)
+
+# Convert log-odds to a probability
+prob = 1 / (1 + exp(-logodds))
+print('Probability %.1f' % prob)
+

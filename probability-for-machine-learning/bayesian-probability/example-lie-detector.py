d39655634b4b8d21221a3cde7e92aca2c68f48bb
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-10 17:54:27 +0700

    Add initial notes for example of calculating probability of a lie during lie detector test

---
+++ probability-for-machine-learning/bayesian-probability/example-lie-detector.py
@@ -0,0 +1,9 @@
+# Consider the case where a person is tested with a lie detector
+# and gets a positive result suggesting that they are lying.
+# What is the probability that the person is actually lying?
+
+# Assumptions
+# P(A) - 2% of people that are tested are lying
+# P(B|A) - When someone is lying, the test can detect them 72% of the time
+# P(not B|not A) - When the machine says someone is not lying, this is true 97% of the time
+

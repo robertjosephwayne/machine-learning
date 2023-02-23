a2da8553827d0fbd8c4c1894ba0ecd1c9a414f68
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-23 16:57:58 +0700

    Add example of multi-output MLP for multivariate time series forecasting

---
+++ deep-learning-time-series-forecasting/multilayer-perceptron/vector-output-model.py
@@ -0,0 +1,52 @@
+# Example of multi-output MLP for multivariate time series forecasting
+from numpy import array
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+# Split a univariate sequence into samples
+def split_sequence(sequence, n_steps_in, n_steps_out):
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        # Find the end of this pattern
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out
+
+        # Check if we are beyond the sequence
+        if out_end_ix > len(sequence):
+            break
+
+        # Gather input and output parts of the pattern
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return array(X), array(y)
+
+
+# Define input sequence
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+# Choose a number of time steps
+n_steps_in, n_steps_out = 3, 2
+
+# Split into samples
+X, y = split_sequence(raw_seq, n_steps_in, n_steps_out)
+
+# Summarize the data
+for i in range(len(X)):
+    print(X[i], y[i])
+
+# Define model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_dim=n_steps_in))
+model.add(Dense(n_steps_out))
+model.compile(optimizer='adam', loss='mse')
+
+# Fit model
+model.fit(X, y, epochs=2000, verbose=0)
+
+# Demonstrate prediction
+x_input = array([70, 80, 90])
+x_input = x_input.reshape((1, n_steps_in))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)

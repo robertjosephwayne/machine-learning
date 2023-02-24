3178e074a5247da46f0f1efb0f694bd13746cc70
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-24 16:25:39 +0700

    Add multivariate multi-step 1D CNN example

---
+++ deep-learning-time-series-forecasting/convolutional-neural-network/multivariate/multi-step/multiple-input.py
@@ -0,0 +1,63 @@
+# Multivariate multi-step 1D CNN example
+from numpy import array, hstack
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.layers.convolutional import Conv1D, MaxPooling1D
+
+
+# Split a multivariate sequence into samples
+def split_sequences(sequences, n_steps_in, n_steps_out):
+    X, y = list(), list()
+    for i in range(len(sequences)):
+        # Find the end of this pattern
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out - 1
+        # Check if we are beyond the dataset
+        if out_end_ix > len(sequences):
+            break
+        # Gather input and output parts of the pattern
+        seq_x, seq_y = sequences[i:end_ix, :-1], sequences[end_ix - 1:out_end_ix, -1]
+        X.append(seq_x)
+        y.append(seq_y)
+    return array(X), array(y)
+
+
+# Define input sequence
+in_seq1 = array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+in_seq2 = array([15, 25, 35, 45, 55, 65, 75, 85, 95])
+out_seq = array([in_seq1[i] + in_seq2[i] for i in range(len(in_seq1))])
+
+# Convert to [rows, columns] structure
+in_seq1 = in_seq1.reshape((len(in_seq1), 1))
+in_seq2 = in_seq2.reshape((len(in_seq2), 1))
+out_seq = out_seq.reshape((len(out_seq), 1))
+
+# Horizontally stack columns
+dataset = hstack((in_seq1, in_seq2, out_seq))
+
+# Choose a number of time steps
+n_steps_in, n_steps_out = 3, 2
+
+# Convert into input/output
+X, y = split_sequences(dataset, n_steps_in, n_steps_out)
+
+# The dataset knows the number of features, e.g. 2
+n_features = X.shape[2]
+
+# Define model
+model = Sequential()
+model.add(Conv1D(64, 2, activation='relu', input_shape=(n_steps_in, n_features)))
+model.add(MaxPooling1D())
+model.add(Flatten())
+model.add(Dense(50, activation='relu'))
+model.add(Dense(n_steps_out))
+model.compile(optimizer='adam', loss='mse')
+
+# Fit model
+model.fit(X, y, epochs=2000, verbose=0)
+
+# Demonstrate prediction
+x_input = array([[70, 75], [80, 85], [90, 95]])
+x_input = x_input.reshape((1, n_steps_in, n_features))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
